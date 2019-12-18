cd9d6a498bd0b560ca7fc839b17be51436ba753e
LHR-lhr <lhr180107@gmail.com>
2019-12-18 15:59:06 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Top />
+    <!-- <Contai/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Top from './components/Top.vue'
+// import Center from './components/container.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Top:Top,
+    // Contai:Center
   }
 }
 </script>
 
 <style>
+*{
+  margin:0;
+  padding:0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
+  min-height: 100vh;
+  /* background: rgba(51,166,184,.3); */
+  position:relative;
+}
+#app:before{
+  content:'';
+  position:absolute;
+  top:0;
+  left:0;
+  width:100%;
+  height:100%;
+  background:url('./assets/bg1.jpg') no-repeat 0 0 ;
+  background-size:100vw auto;
+  background-attachment:fixed;
+  filter:blur(15px);
+  z-index: -2;
+}
+#app:after{
+  content:'';
+  position:absolute;
+  top:0;
+  left:0;
+  width:100%;
+  height:100%;
+  background:rgba(128,128,128,.2) ;
+  z-index: -1;
+  opacity:.4;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import './assets/css/font-awesome.css'
+
+Vue.prototype.$axios = axios
+// axios.defaults.baseURL = '/api'
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 Vue.config.productionTip = false
 
